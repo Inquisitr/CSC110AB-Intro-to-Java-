489a0c374a52ec3857fb58b102a18ab4a68a0238
Austin Newton <62123277+AustinNewtonCode@users.noreply.github.com>
2021-04-07 13:14:23 -0700

    Add files via upload

---
+++ FirstProgram2.java
@@ -0,0 +1,25 @@
+/*
+FirstProgram2
+By Austin Newton
+01/25/2021
+ */
+package firstprogram2;
+
+/**
+ *
+ * @author anewt
+ */
+public class FirstProgram2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        System.out.print("Austin ");
+        System.out.print("Newton\n");
+        System.out.println("The sum is: " + (5+9));
+    }//end main()
+    
+}
